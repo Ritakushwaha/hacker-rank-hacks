2b3982577a3d4bafd4fa935a544e6b8f8864244e
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-01-25 10:54:09 +0530

    Type Casting Integer to String in Java

---
+++ Prepare/Java/Introduction/JavaIntToString.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaIntToString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String n1 = String.valueOf(n);
+
+        if(n1.equals(Integer.toString(n))){
+            System.out.println("Good job");
+        }
+        else{
+            System.out.println("Wrong answer");
+        }
+    }
+}
