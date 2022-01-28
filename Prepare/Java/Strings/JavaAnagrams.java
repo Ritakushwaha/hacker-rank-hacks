c6cda919961104dcc000da7544a4b82348946ae3
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-01-28 12:12:41 +0530

    Check if two strings are anagrams in JAVA using HashMap

--- Prepare/Java/Strings/JavaAnagrams.java
+++ Prepare/Java/Strings/JavaAnagrams.java
@@ -49,3 +49,82 @@ public class JavaAnagrams {
     }
 }
 
+
+
+/** Second Solution */
+
+/*
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        
+        try{
+            if( a == null || b == null || a.equals("") || b.equals("")){
+                throw new IllegalArgumentException();
+            }
+
+            if ( a.length() != b.length()){
+                return false;
+            }
+            else{
+                a = a.toLowerCase();
+                b = b.toLowerCase();
+
+                // populate a map with letters and frequencies of String b
+                Map<Character, Integer> map = new HashMap<>();
+
+                for (int k = 0; k < b.length(); k++){
+                    char letter = b.charAt(k);    
+
+                    if( ! map.containsKey(letter)){
+                        map.put( letter, 1 );
+                    } else {
+                        Integer frequency = map.get( letter );
+                        map.put( letter, ++frequency );
+                    }
+                }
+        
+                for (int k = 0; k < a.length(); k++){
+                    char letter = a.charAt(k);
+
+                    if( ! map.containsKey( letter ) )
+                        return false;
+
+                    Integer frequency = map.get( letter );
+
+                    if( frequency == 0 )
+                        return false;
+                    else    
+                        map.put( letter, --frequency);
+                }
+            // if the code got that far it is an anagram
+            return true;
+            }
+        }
+        catch(IllegalArgumentException i){
+        System.exit(0);
+        }
+        return true;
+    }
+    
+    
+        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine().toLowerCase();
+        String b = sc.nextLine().toLowerCase();
+        sc.close();
+    
+        if(isAnagram(a,b)){
+            System.out.println("Anagrams");
+        }else{
+            System.out.println("Not Anagrams");
+        }
+    }
+}
+*/
+
