aea03ad0e0d0a5853a1bcc8cfb3d339165980b85
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-02-05 20:48:46 +0530

    Java Regex for valid IP address

---
+++ Prepare/Java/Strings/JavaRegex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class MyRegex{
+    public String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+}
+
+public class JavaRegex {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+        in.close();
+    }
+}
