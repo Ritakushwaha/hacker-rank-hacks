602eb96f98d19319b3c594974c022164d910d0ba
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-02-08 11:40:55 +0530

    Class and Instance Variables in Python

--- Prepare/30DaysOfCode/Tutorials/Day4:ClassVsInstance.py
+++ Prepare/30DaysOfCode/Tutorials/Day4:ClassVsInstance.py
@@ -5,3 +5,35 @@ Created on Mon Jan 17 13:06:19 2022
 
 @author: rita
 """
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        if initialAge < 0:
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
+        else:
+            self.age = initialAge
+
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if age < 13:
+            print("You are young.")
+        elif 13 <= age < 18:
+            print("You are a teenager.")
+        elif age >= 18:
+            print("You are old.")
+
+    def yearPasses(self):
+        # Increment the age of the person in here
+        global age
+        age += 1
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
