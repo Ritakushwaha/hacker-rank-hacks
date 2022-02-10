e022b0cb40959f3beddcebadb4b603fa6296b8b6
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-02-10 16:17:15 +0530

    Day 6 : Lets review

---
+++ Prepare/30DaysOfCode/Tutorials/Day6:LetsReview.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    n = int(input())
+    for i in range(n):
+        s = input()
+        s1 = ""
+        s2 = ""
+        for i in range(len(s)):
+            if i%2 == 0:
+                s1 += s[i]
+            else:
+                s2 += s[i]
+        print(s1,s2)
