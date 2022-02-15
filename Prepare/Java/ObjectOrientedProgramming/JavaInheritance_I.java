cbf572cb9e5ad457a5bcd8da396cdb4860b7b8c8
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-02-16 00:23:45 +0530

    Java Inheritance I

---
+++ Prepare/Java/ObjectOrientedProgramming/JavaInheritance_I.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+    
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+public class JavaInheritance_I {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Bird bird = new Bird();
+      bird.walk();
+      bird.fly();
+      bird.sing();
+    }
+}
