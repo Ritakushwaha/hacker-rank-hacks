6d95c629c79266237f059bf11a5c658fc0be1fb9
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-02-20 22:12:59 +0530

    Java Exception Handling

--- Prepare/Java/Exception Handling/JavaExceptionHandling.java
+++ Prepare/Java/Exception Handling/JavaExceptionHandling.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 
 public class JavaExceptionHandling {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-
-            MyCalculator mc = new MyCalculator();
-            try {
-                System.out.println(mc.power(a, b));
-            } catch (Exception e) {
-                System.out.println(e);
+    
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            while(sc.hasNextInt()){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+    
+                MyCalculator mc = new MyCalculator();
+                try {
+                    System.out.println(mc.power(a, b));
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+            sc.close();
+        }
+    }
+    
+    class MyCalculator{
+        long power(int n, int p) throws Exception{
+            if(n<0 || p<0){
+                throw new Exception("n or p should not be negative.");
+            }else if(n==0 && p==0){
+                throw new Exception("n and p should not be zero.");
+            }else{
+                return (long) Math.pow(n, p);
             }
         }
-        sc.close();
     }
-}
-
-
-class MyCalculator{
-    long power(int n, int p) throws Exception{
-        if(n<0 || p<0){
-            throw new Exception("n or p should not be negative.");
-        }else if(n==0 || p==0){
-            throw new Exception("n and p should n be zero.");
-        }else{
-            return (long) Math.pow(n, p);
-        }
-    }
-}
+    
