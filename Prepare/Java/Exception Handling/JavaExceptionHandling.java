067776e603595c819f6bbe0933d739b5e2b6e70d
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-02-20 22:10:47 +0530

    Java Exception Handling

--- Prepare/Java/Exception Handling/JavaExceptionHandling.java
+++ Prepare/Java/Exception Handling/JavaExceptionHandling.java
@@ -3,31 +3,28 @@ import java.util.Scanner;
 public class JavaExceptionHandling {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        sc.close();
-        MyCalculator mc = new MyCalculator(a,b);
-        try {
-            System.out.println(mc.power(a, b));
-        } catch (Exception e) {
-            System.out.println(e);
+        while(sc.hasNextInt()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            MyCalculator mc = new MyCalculator();
+            try {
+                System.out.println(mc.power(a, b));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         }
+        sc.close();
     }
 }
 
 
 class MyCalculator{
-    int n,p;
-    public MyCalculator(int a, int b) {
-        this.n = a;
-        this.p = b;
-    }
-
     long power(int n, int p) throws Exception{
         if(n<0 || p<0){
             throw new Exception("n or p should not be negative.");
         }else if(n==0 || p==0){
-            throw new Exception("n or p should n be zero.");
+            throw new Exception("n and p should n be zero.");
         }else{
             return (long) Math.pow(n, p);
         }
