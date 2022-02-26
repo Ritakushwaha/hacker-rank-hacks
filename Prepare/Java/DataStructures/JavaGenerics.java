c399b70d3bf32f210cb3cf26cf502af05cd13768
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-02-26 23:26:29 +0530

    Java Generics

--- Prepare/Java/DataStructures/JavaGenerics.java
+++ Prepare/Java/DataStructures/JavaGenerics.java
@@ -1,3 +1,19 @@
-public class JavaGeneric {
-    
+class Printer{
+    <T> void printArray(T[] arr){
+        for(T item:arr){
+            System.out.println(item);
+        }
+    }
+}
+
+public class JavaGenerics {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        Printer p = new Printer();
+        p.printArray(intArray);
+        p.printArray(stringArray);
+    }
 }
