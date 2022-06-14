8623d4ace244158aec1a228092c92284888d77d4
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-06-14 22:31:43 +0530

    Breaking The Records

---
+++ Prepare/InterviewPreparationKits/3MonthsPreparationKit/Week1/TimeConversion.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+
+    hh=int(s[:2])
+    print(hh)
+    if "PM" in s:
+        if hh!=12:
+            hh+=12
+
+    if "AM" in s:
+        if hh==12:
+            hh=00
+    
+    hour=(str(hh))
+    
+    return f"{hour}{s[2:-2]}"
+
+if __name__ == '__main__':
+    s = input()
+    print(timeConversion(s))
+
