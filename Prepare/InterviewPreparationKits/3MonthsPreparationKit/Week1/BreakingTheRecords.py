f415e79ba8f028598192bcb5c0b4ce9f2e1eab5c
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-06-15 23:51:54 +0530

    commit

--- Prepare/InterviewPreparationKits/3MonthsPreparationKit/Week1/BreakingTheRecords.py
+++ Prepare/InterviewPreparationKits/3MonthsPreparationKit/Week1/BreakingTheRecords.py
@@ -1,23 +1,9 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'breakingRecords' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY scores as parameter.
-#
-
 def breakingRecords(scores):
-    # Write your code here
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
 
@@ -25,7 +11,4 @@ if __name__ == '__main__':
 
     result = breakingRecords(scores)
 
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
+    print(result)
